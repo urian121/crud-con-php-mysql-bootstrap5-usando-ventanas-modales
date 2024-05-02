18c75b689af9462c251c2336ff43a004285e7ed8
URIAN VIERA <urian1213viera@gmail.com>
2024-05-01 20:43:15 -0500

    Update modalEditar.php

--- modales/modalEditar.php
+++ modales/modalEditar.php
@@ -2,7 +2,7 @@
         <div class="modal-dialog modal-dialog-centered">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h1 class="modal-title fs-5 titulo_modal">Registrar Nuevo Empleado</h1>
+                    <h1 class="modal-title fs-5 titulo_modal">Actualizar Información Empleado</h1>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
